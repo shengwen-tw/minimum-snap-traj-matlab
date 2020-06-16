b2603527b54c14b253cd25fa9adbba678fd12a3d
Shengwen Cheng <shengwen1997.tw@gmail.com>
2020-06-16 12:56:45 +0800

    complete motion planner for 3D space

--- main.m
+++ main.m
@@ -1,52 +1,20 @@
-traj_gen = trajectory_generator;
+mp = motion_planner;
 
-%trajectory parameters
-traj_size = 3;
-%
-waypoints(1) = 0.5;
-waypoints(2) = 1.0;
-waypoints(3) = 2.0;
-waypoints(4) = 4.0;
-%
-flight_times(1) = 1.0;
-flight_times(2) = 1.0;
-flight_times(3) = 1.0;
+traj_num = 4;
 
-%trajectory planning
-traj_coeff_list = traj_gen.plan_optimized_segment(waypoints, flight_times, traj_size);
+%waypoints
+waypoints(1, :) = [0.0, 0.0, 0.0];
+waypoints(2, :) = [0.6, -0.6, 0.0];
+waypoints(3, :) = [0.0, 0.6, 0.0];
+waypoints(4, :) = [-0.6, -0.6, 0.0];
+waypoints(5, :) = [0.0, 0.0, 0.0];
 
-%get total flight time
-total_flight_time = traj_gen.get_total_flight_time(flight_times, traj_size);
+%trajectory flight times
+traj_flight_times(1) = 1;
+traj_flight_times(2) = 1;
+traj_flight_times(3) = 1;
+traj_flight_times(4) = 1;
 
-%create arrays for plotting
-PLOT_TIMES_PER_SECOND = 50;
-ITERATION_TIMES = total_flight_time * PLOT_TIMES_PER_SECOND;
-time_arr = zeros(1, ITERATION_TIMES);
-traj_arr = zeros(1, ITERATION_TIMES);
-
-%plot trajectories
-elapsed_index = 0;
-for i = 1: traj_size
-    %calculate i-th trajectory plot times and time step
-    traj_plot_times = traj(i).t * PLOT_TIMES_PER_SECOND;
-    time_step = traj(i).t / traj_plot_times;
-    
-    %plot i-th trajectory
-    for j = 1: traj_plot_times
-        %update trajectory array
-        traj_arr(elapsed_index + j) = traj_gen.calc_7th_polynomial(traj_coeff_list(i, :), (j-1) * time_step);
-        
-        %update elapsed time array
-        time_arr(elapsed_index + j) = (elapsed_index + j - 1) * time_step;
-    end
-    
-    %accumlate total elapsed time
-    elapsed_index = elapsed_index + traj_plot_times;
-end
-
-figure('Name', 'trajectory');
-plot(time_arr, traj_arr);
-xlabel('time [s]');
-ylabel('x position [m]');
-pause;
-close all;
+mp = init(mp, waypoints, traj_flight_times, traj_num);
+mp = plan_trajectory(mp);
+plot_trajectories(mp);
--- motion_planner.m
+++ motion_planner.m
@@ -1,149 +1,120 @@
-classdef motion_planner    
+classdef motion_planner
+    properties
+        PLOT_TIMES_PER_SECOND = 50;
+        ITERATION_TIMES;
+        
+        traj_size;
+        flight_times;
+        
+        x_traj_coeffs;
+        x_waypoints;
+        
+        y_traj_coeffs;
+        y_waypoints;
+        
+        z_traj_coeffs;
+        z_waypoints;
+    end
+    
     methods
-        function A_end = generate_A_end_matrix(obj, t)
-            A_end = [1 t t^2  t^3   t^4    t^5    t^6     t^7;
-                     0 1 2*t  3*t^2 4*t^3  5*t^4  6*t^5   7*t^6;
-                     0 0 2    6*t   12*t^2 20*t^3 30*t^4  42*t^5;
-                     0 0 0    6     24*t   60*t^2 120*t^3 210*t^4];
-        end
-        
-        function Q = generate_Q_matrix(obj, t)
-            b0 = 24;
-            b1 = 120;
-            b2 = 360;
-            b3 = 840;
-            Q = zeros(8, 8);
-            Q(5, 5) = b0 * b0 * t;
-            Q(5, 6) = b0 * b1 * (1/2) * t^2;
-            Q(5, 7) = b0 * b2 * (1/3) * t^3;
-            Q(5, 8) = b0 * b3 * (1/4) * t^4;
-            Q(6, 5) = b0 * b1 * (1/2) * t^2;
-            Q(6, 6) = b1 * b1 * (1/3) * t^3;
-            Q(6, 7) = b1 * b2 * (1/4) * t^4;
-            Q(6, 8) = b1 * b3 * (1/5) * t^5;
-            Q(7, 5) = b0 * b2 * (1/3) * t^3;
-            Q(7, 6) = b1 * b2 * (1/4) * t^4;
-            Q(7, 7) = b2 * b2 * (1/5) * t^5;
-            Q(7, 8) = b2 * b3 * (1/6) * t^6;
-            Q(8, 5) = b0 * b3 * (1/4) * t^4;
-            Q(8, 6) = b1 * b3 * (1/5) * t^5;
-            Q(8, 7) = b2 * b3 * (1/6) * t^6;
-            Q(8, 8) = b3 * b3 * (1/7) * t^7;
-        end
-        
-        function traj_polys = plan_optimized_segment(obj, waypoints, t, traj_size)
-            H = zeros(traj_size*8, traj_size*8);
+        function ret_obj = init(obj, waypoints, flight_times, traj_size)
+            traj_gen = trajectory_generator;
             
-            %construct hessian matrix
-            for i = 1: traj_size
-                Q = generate_Q_matrix(obj, t(i));
-                %disp(Q);
+            obj.traj_size = traj_size;
+            obj.flight_times = flight_times;
+            
+            obj.x_waypoints = waypoints(1:traj_size+1, 1);
+            obj.y_waypoints = waypoints(1:traj_size+1, 2);
+            obj.z_waypoints = waypoints(1:traj_size+1, 3);
+            
+            ret_obj = obj;
+        end
+        
+        function ret_obj = plan_trajectory(obj)
+            traj_gen = trajectory_generator;
+            
+            %size(obj.x_waypoints)
+            %size(obj.flight_times)
+            %disp(obj.traj_size)
+            
+            %trajectory planning
+            obj.x_traj_coeffs = traj_gen.plan_optimized_segment(obj.x_waypoints, obj.flight_times, obj.traj_size);
+            obj.y_traj_coeffs = traj_gen.plan_optimized_segment(obj.y_waypoints, obj.flight_times, obj.traj_size);
+            obj.z_traj_coeffs = traj_gen.plan_optimized_segment(obj.z_waypoints, obj.flight_times, obj.traj_size);
+            
+            %disp(obj.x_traj_coeffs);
+            %disp(obj.y_traj_coeffs);
+            %disp(obj.z_traj_coeffs);
+            
+            ret_obj = obj;
+        end
+        
+        function [traj_arr, time_arr]=plot_1d_trajectory(obj, traj_coeff)
+            traj_gen = trajectory_generator;
+
+            traj_arr = zeros(1, obj.ITERATION_TIMES);
+            time_arr = zeros(1, obj.ITERATION_TIMES);
+            
+            %plot trajectories
+            elapsed_index = 0;
+            for i = 1: obj.traj_size
+                %calculate i-th trajectory plot times and time step
+                traj_plot_times = obj.flight_times(i) * obj.PLOT_TIMES_PER_SECOND;
+                time_step = obj.flight_times(i) / traj_plot_times;
                 
-                r = (i-1)*8 + 1;
-                H(r:r+7, r:r+7) = Q;
-            end
-            
-            %construct equility constraints
-            A_start = [1 0 0 0 0 0 0 0;
-                       0 1 0 0 0 0 0 0;
-                       0 0 2 0 0 0 0 0;
-                       0 0 0 6 0 0 0 0];
-              
-            A_next = [0  0  0  0 0 0 0 0;
-                      0 -1  0  0 0 0 0 0;
-                      0  0 -2  0 0 0 0 0;
-                      0  0  0 -6 0 0 0 0];
-            
-            A_interior_point = [1 0 0 0 0 0 0 0];
-                  
-            %generate A matrix
-            wp_cnt = traj_size + 1;
-            A = zeros(wp_cnt*4 + (traj_size-1), (wp_cnt - 1) * 8);
-            for i = 1: traj_size
-                if i == 1
-                    A_end = generate_A_end_matrix(obj, t(i));
+                %plot i-th trajectory
+                for j = 1: traj_plot_times
+                    %update trajectory array
+                    traj_arr(elapsed_index + j) = traj_gen.calc_7th_polynomial(traj_coeff(i, :), (j-1) * time_step);
                     
-                    start_r = 1;
-                    end_r = 1 + 4;
-                    c = 1;                    
-                    A(start_r:start_r+3, c:c+7) = A_start;
-                    A(end_r:end_r+3, c:c+7) = A_end;
-                else
-                    A_end = generate_A_end_matrix(obj, t(i));
-                    
-                    next_r = ((i-1)*4) + 1;
-                    end_r = (i*4) + 1;
-                    c = ((i-1)*8) + 1;
-                    A(next_r:next_r+3, c:c+7) = A_next;
-                    A(end_r:end_r+3, c:c+7) = A_end;
-                    
-                    %disp(next_r)
-                    %disp(end_r)
-                    %disp(c)
+                    %update elapsed time array
+                    time_arr(elapsed_index + j) = (elapsed_index + j - 1) * time_step;
                 end
-            end
-            %
-            for i = 1: (traj_size-1)
-                r = wp_cnt*4 + i;
-                c = i*8 + 1;
-                A(r, c:c+7) = A_interior_point;
                 
-                %disp(r);
-                %disp(c);
+                %accumlate total elapsed time
+                elapsed_index = elapsed_index + traj_plot_times;
             end
-            
-            d = zeros(4*wp_cnt + traj_size-1, 1);
-            for i = 1: traj_size
-                start_r = (i - 1) * 4;
-                d(start_r+1) = waypoints(i); %position
-                d(start_r+2) = 0;                 %velocity
-                d(start_r+3) = 0;                 %acceleration
-                d(start_r+4) = 0;                 %jerk
-                
-                end_r = i * 4;
-                d(end_r+1) = waypoints(i+1); %position
-                d(end_r+2) = 0;                   %velocity
-                d(end_r+3) = 0;                   %acceleration
-                d(end_r+4) = 0;                   %jerk
-            end
-            %
-            for i = 1: (traj_size-1)
-                r = wp_cnt*4 + i;
-                d(r) = waypoints(i+1);
-            end
-            
-            %disp(H)
-            %disp(A)
-            %disp(d)
-            
-            %size(H)
-            %size(A)
-            %size(d)
-            
-            traj_polys = quadprog(H, [], [], [], A, d);
         end
         
-        function traj_coeffs=get_traj_coeff_from_list(obj, index, coeff_list)
-            new_index = (index - 1) * 8;
-            traj_coeffs = [coeff_list(new_index+1);
-                           coeff_list(new_index+2);
-                           coeff_list(new_index+3);
-                           coeff_list(new_index+4);
-                           coeff_list(new_index+5);
-                           coeff_list(new_index+6);
-                           coeff_list(new_index+7);
-                           coeff_list(new_index+8)];
-        end
-        
-        function result=calc_7th_polynomial(obj, c, t)
-            result = c(1) + ...
-                     c(2)*t + ...
-                     c(3)*t^2 + ...
-                     c(4)*t^3 + ...
-                     c(5)*t^4 + ...
-                     c(6)*t^5 + ...
-                     c(7)*t^6 + ...
-                     c(8)*t^7;
+        function plot_trajectories(obj)
+            traj_gen = trajectory_generator;
+            total_flight_time = traj_gen.get_total_flight_time(obj.flight_times, obj.traj_size);
+            obj.ITERATION_TIMES = total_flight_time * obj.PLOT_TIMES_PER_SECOND;
+            
+            [x_traj_arr, time_arr] = plot_1d_trajectory(obj, obj.x_traj_coeffs);
+            [y_traj_arr, time_arr] = plot_1d_trajectory(obj, obj.y_traj_coeffs);
+            [z_traj_arr, time_arr] = plot_1d_trajectory(obj, obj.z_traj_coeffs);
+                        
+            figure('Name', 'trajectory');
+            %
+            subplot (3, 1, 1);
+            plot(time_arr, x_traj_arr);
+            xlabel('time [s]');
+            ylabel('x position [m]');
+            grid on;
+            %
+            subplot (3, 1, 2);
+            plot(time_arr, y_traj_arr);
+            xlabel('time [s]');
+            ylabel('y position [m]');
+            grid on;
+            %
+            subplot (3, 1, 3);
+            plot(time_arr, z_traj_arr);
+            xlabel('time [s]');
+            ylabel('z position [m]');
+            grid on;
+
+            figure('Name', 'trajectory x-y');
+            plot(x_traj_arr, y_traj_arr);
+            grid on;
+            
+            figure('Name', 'trajectory x-y-z');
+            plot3(x_traj_arr, y_traj_arr, z_traj_arr);
+            grid on;
+            
+            pause;
+            close all;
         end
     end
 end
